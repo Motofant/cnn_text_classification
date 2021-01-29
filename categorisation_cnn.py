240910132d28879551073da973918e53b66d60ef
Motofant <49334149+Motofant@users.noreply.github.com>
2021-01-29 22:04:43 +0100

    stückwerk changed outputlayer to a signle one, tests got way worse.

--- categorisation_cnn.py
+++ categorisation_cnn.py
@@ -16,7 +16,9 @@ tf.compat.v1.disable_eager_execution() # thanks to anugrahasinha, https://github
 #### Variables ####
 
 # necessary for beta-version
+input_file = "C:/Users/Erik/Documents/Uni/BA/Repo/cnn_text_classification/output/defaulttwo_t.csv"# ordinal encoded input
 input_file = "C:/Users/Erik/Documents/Uni/BA/Repo/cnn_text_classification/output/defaulttest.csv"# ordinal encoded input
+
 #cat_input = "C:/Users/Erik/Desktop/cnn_testinput_cat.csv"
 #np.set_printoptions(threshold=sys.maxsize)
 
@@ -102,16 +104,17 @@ with open(input_file, mode = "r", newline = "\n") as text_file:
     reader = csv.reader(text_file, delimiter= "\t",  quotechar='"', quoting=csv.QUOTE_MINIMAL )
     i = 0
     for row in reader:
-        input_categories=np.append(input_categories,p.oneHot(row[0],cat_size, 1))
-        
+        #input_categories=np.append(input_categories,p.oneHot(row[0],cat_size, 1))
+        input_categories = np.append(input_categories,int(float(row[0])))
         numbers = []
         for item in row[1:]:
-            numbers.append(int(item))
+            numbers.append(int(float(item)))
         input_text= np.append(input_text,numbers)
         i +=1
 
 input_text = np.array(input_text.reshape((int(input_text.shape[0]/Text_length),Text_length,word_vec_length))) # 1 has to be varaible for one Hot 
-k = makeMore(input_categories, cat_size)
+#k = makeMore(input_categories, cat_size)
+k = input_categories
 
 ## define input
 
@@ -122,17 +125,24 @@ k = makeMore(input_categories, cat_size)
 # https://blog.keras.io/using-pre-trained-word-embeddings-in-a-keras-model.html
 # https://keras.io/examples/nlp/pretrained_word_embeddings/
 
+inp = keras.Input(nn_input_size)
+outz = keras.utils.to_categorical(k,cat_size)
+print(outz)
+model = Sequential()
+#model = keras.Model(inp, [out_0,out_1,out_2,out_3,out_4,out_5,out_6,out_7,out_8])
+#model = keras.Model(inp,np.array(k))
+model.add(Conv1D(64,3,input_shape=(1200,1), activation="tanh",padding="valid"))
+model.add(MaxPooling1D(2,data_format="channels_first"))
+model.add(Conv1D(64,3, activation="tanh",padding="valid"))
+model.add(MaxPooling1D(2,data_format="channels_first"))
+model.add(Conv1D(64,3, activation="tanh",padding="valid"))
+#model.add(MaxPooling1D(2,data_format="channels_first"))
+model.add(GlobalMaxPooling1D(data_format="channels_first"))
+model.add(Dropout(0.5)) #ony for testing needed -> for limited datasets
+model.add(Dense(9, activation="softmax"))
+
 
 '''
-model.add(Conv1D(32,3,input_shape=(1200,1), activation="relu",padding="same"))
-model.add(MaxPooling1D(2,data_format="channels_first"))
-model.add(Conv1D(16,3, activation="relu",padding="same"))
-model.add(MaxPooling1D(2,data_format="channels_first"))
-model.add(Conv1D(8,3, activation="relu"))
-model.add(GlobalMaxPooling1D())
-model.add(Dense(1, activation="relu"))
-'''
-
 inp = keras.Input(nn_input_size)
 
 x = Conv1D(64,3,activation="sigmoid",padding="valid")(inp)
@@ -154,11 +164,11 @@ out_5 = Dense(1, activation="sigmoid")(x)
 out_6 = Dense(1, activation="sigmoid")(x)
 out_7 = Dense(1, activation="sigmoid")(x)
 out_8 = Dense(1, activation="sigmoid")(x)
+'''
 
-#model.add(Dropout(0.5)) #ony for testing needed -> for limited datasets
 
-model = keras.Model(inp, [out_0,out_1,out_2,out_3,out_4,out_5,out_6,out_7,out_8])
-
+#model = keras.Model(inp, [out_0,out_1,out_2,out_3,out_4,out_5,out_6,out_7,out_8])
+#model = keras.Model(inp, k)
 
 
 
@@ -167,19 +177,23 @@ model = keras.Model(inp, [out_0,out_1,out_2,out_3,out_4,out_5,out_6,out_7,out_8]
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'],experimental_run_tf_function=False)
 print("compiled succesfully")
 #model.fit(x = input_text,y =[k[0],k[1],k[2],k[3],k[4],k[5],k[6],k[7],k[8]],epochs=50, batch_size=450)
-#model.fit(x = input_text,y =np.array(k),epochs=10, batch_size=9)
-history = model.fit(x = input_text,y =[np.array(k[0]),np.array(k[1]),np.array(k[2]),np.array(k[3]),np.array(k[4]),np.array(k[5]),np.array(k[6]),np.array(k[7]),np.array(k[8])],epochs=50, batch_size=450)
+history =model.fit(x = input_text,y =outz,epochs=20, batch_size=450)
+#history = model.fit(x = input_text,y =[np.array(k[0]),np.array(k[1]),np.array(k[2]),np.array(k[3]),np.array(k[4]),np.array(k[5]),np.array(k[6]),np.array(k[7]),np.array(k[8])],epochs=50, batch_size=450)
 
 model.summary()
 ## output
-'''
-_, accuracy = model.evaluate(input_text,[np.array(k[0]),np.array(k[1]),np.array(k[2]),np.array(k[3]),np.array(k[4]),np.array(k[5]),np.array(k[6]),np.array(k[7]),np.array(k[8])])
+
+accuracy = model.evaluate(input_text,outz)
 print('Accuracy: %.2f' % (accuracy*100))
 
-predictions = model.predict_classes(input_text)
+predictions = model.predict(input_text)
 for i in range(5):
-	print('%s => %d (expected %d)' % (input_text[i].tolist(), predictions[i], [np.array(k[0]),np.array(k[1]),np.array(k[2]),np.array(k[3]),np.array(k[4]),np.array(k[5]),np.array(k[6]),np.array(k[7]),np.array(k[8])][i]))
-'''
+#print(input_text.tolist())
+    print("prediction: ")
+    print(predictions[i])
+    print("real")
+    print(outz[i])
+
 print(history.history.keys())
 
 
@@ -187,33 +201,34 @@ print(history.history.keys())
 
 
 # test visual
-plt.plot(history.history['dense_accuracy'])
-plt.plot(history.history['dense_1_accuracy'])
-plt.plot(history.history['dense_2_accuracy'])
-plt.plot(history.history['dense_3_accuracy'])
-plt.plot(history.history['dense_4_accuracy'])
-plt.plot(history.history['dense_5_accuracy'])
-plt.plot(history.history['dense_6_accuracy'])
-plt.plot(history.history['dense_7_accuracy'])
-plt.plot(history.history['dense_8_accuracy'])
+plt.plot(history.history['accuracy'])
+# plt.plot(history.history['dense_1_accuracy'])
+# plt.plot(history.history['dense_2_accuracy'])
+# plt.plot(history.history['dense_3_accuracy'])
+# plt.plot(history.history['dense_4_accuracy'])
+# plt.plot(history.history['dense_5_accuracy'])
+# plt.plot(history.history['dense_6_accuracy'])
+# plt.plot(history.history['dense_7_accuracy'])
+# plt.plot(history.history['dense_8_accuracy'])
 
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
-plt.legend(['dense_accuracy', 'dense_1_accuracy', 'dense_2_accuracy', 'dense_3_accuracy', 'dense_4_accuracy', 'dense_5_accuracy', 'dense_6_accuracy', 'dense_7_accuracy', 'dense_8_accuracy'], loc='lower right')
-plt.show()
+plt.legend(['dense_accuracy'], loc='lower right')
+#plt.show()
+
 # summarize history for loss
-plt.plot(history.history['dense_loss'])
-plt.plot(history.history['dense_1_loss'])
-plt.plot(history.history['dense_2_loss'])
-plt.plot(history.history['dense_3_loss'])
-plt.plot(history.history['dense_4_loss'])
-plt.plot(history.history['dense_5_loss'])
-plt.plot(history.history['dense_6_loss'])
-plt.plot(history.history['dense_7_loss'])
-plt.plot(history.history['dense_8_loss'])
+plt.plot(history.history['loss'])
+# plt.plot(history.history['dense_1_loss'])
+# plt.plot(history.history['dense_2_loss'])
+# plt.plot(history.history['dense_3_loss'])
+# plt.plot(history.history['dense_4_loss'])
+# plt.plot(history.history['dense_5_loss'])
+# plt.plot(history.history['dense_6_loss'])
+# plt.plot(history.history['dense_7_loss'])
+# plt.plot(history.history['dense_8_loss'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['dense_loss', 'dense_1_loss', 'dense_2_loss', 'dense_3_loss', 'dense_4_loss', 'dense_5_loss', 'dense_6_loss', 'dense_7_loss', 'dense_8_loss'], loc='upper right')
-plt.show()
+plt.legend(['dense_loss'], loc='upper right')
+#plt.show()
